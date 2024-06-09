9501cd256390fd16eae45086bc96bfbbf48eab61
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-06-09 00:30:55 -0300

    refactor: split scripts to download version data and list asset indexes

--- download_manifests.py
+++ download_manifests.py
@@ -20,9 +20,6 @@ version_manifest = manifest_data["versions"]
 with open("version_manifest.json", "w") as f:
     json.dump(manifest_data, f, indent=4)
 
-# Create a dict with the version ID as keys and version data as values
-version_manifest_dict = {version["id"]: version for version in version_manifest}
-
 print(f"The latest release version of Minecraft: Java Edition is {latest_release}.")
 
 # Create a folder called manifest
@@ -47,51 +44,3 @@ for version in version_manifest:
 
     with open(f"manifest/{v_id}.json", "w") as f:
         f.write(manifest)
-
-print()
-
-asset_indexes = {}
-asset_indexes_grouped = {}
-
-# List all files in manifest folder
-for version in shutil.os.listdir("manifest"):
-    with open(f"manifest/{version}", "r") as f:
-        data = json.load(f)
-
-    version = version.replace(".json", "")
-    asset_index = data["assets"]
-
-    if asset_index not in asset_indexes_grouped:
-        asset_indexes_grouped[asset_index] = []
-        print(f"Asset Index: {asset_index}")
-    asset_indexes_grouped[asset_index].append(version)
-
-    asset_indexes[version] = asset_index
-
-
-with open("asset_indexes_grouped.json", "w") as f:
-    json.dump(asset_indexes_grouped, f, indent=4)
-
-with open("asset_indexes.json", "w") as f:
-    json.dump(asset_indexes, f, indent=4)
-
-
-# Filter snapshot versions
-
-asset_indexes_grouped_nosnapshots = {}
-
-for group, group_versions in asset_indexes_grouped.items():
-
-    for version in group_versions:
-        version_data = version_manifest_dict[version]
-
-        if group not in asset_indexes_grouped_nosnapshots:
-            asset_indexes_grouped_nosnapshots[group] = []
-
-        if version_data["type"] != "release":
-            continue
-
-        asset_indexes_grouped_nosnapshots[group].append(version)
-
-with open("asset_indexes_grouped_nosnapshots.json", "w") as f:
-    json.dump(asset_indexes_grouped_nosnapshots, f, indent=4)
---
+++ list_asset_indexes.py
@@ -0,0 +1,63 @@
+import json
+import shutil
+import sys
+import requests
+
+
+asset_indexes = {}
+asset_indexes_grouped = {}
+
+
+# Load version manifest
+with open("version_manifest.json", "r") as f:
+    version_manifest = json.load(f)
+
+
+# Create a dict with the version ID as keys and version data as values
+version_manifest_dict = {
+    version["id"]: version for version in version_manifest["versions"]
+}
+
+
+# List all files in manifest folder
+for version in shutil.os.listdir("manifest"):
+    with open(f"manifest/{version}", "r") as f:
+        data = json.load(f)
+
+    version = version.replace(".json", "")
+    asset_index = data["assets"]
+
+    if asset_index not in asset_indexes_grouped:
+        asset_indexes_grouped[asset_index] = []
+        print(f"Asset Index: {asset_index}")
+    asset_indexes_grouped[asset_index].append(version)
+
+    asset_indexes[version] = asset_index
+
+
+with open("asset_indexes_grouped.json", "w") as f:
+    json.dump(asset_indexes_grouped, f, indent=4)
+
+with open("asset_indexes.json", "w") as f:
+    json.dump(asset_indexes, f, indent=4)
+
+
+# Filter snapshot versions
+
+asset_indexes_grouped_nosnapshots = {}
+
+for group, group_versions in asset_indexes_grouped.items():
+
+    for version in group_versions:
+        version_data = version_manifest_dict[version]
+
+        if group not in asset_indexes_grouped_nosnapshots:
+            asset_indexes_grouped_nosnapshots[group] = []
+
+        if version_data["type"] != "release":
+            continue
+
+        asset_indexes_grouped_nosnapshots[group].append(version)
+
+with open("asset_indexes_grouped_nosnapshots.json", "w") as f:
+    json.dump(asset_indexes_grouped_nosnapshots, f, indent=4)
